41994f9ba39f5fb0f9827073379f612888418687
Chakrit Wichian <service@chakrit.net>
2014-06-17 23:54:33 +0700

    Minor refactoring.

---
+++ context.go
@@ -0,0 +1,31 @@
+package main
+
+type Context struct {
+	table         Table
+	depth         int
+	cache         Cache
+	lastMove      Move
+	possibleMoves []Move
+	solution      MoveList
+}
+
+func NewContext(table Table, depth int) *Context {
+	return &Context{
+		table: table,
+		depth: depth,
+		cache: NewCache(),
+		lastMove: EmptyMove,
+		possibleMoves: GenerateMoveList(table),
+		solution: EmptyMoveList,
+	}
+}
+
+func (c *Context) AppendSolution(table Table, move Move) *Context {
+	child := &Context{}
+	*child = *c
+	child.table = table
+	child.depth -= 1
+	child.lastMove = move
+	child.solution = append(c.solution, move)
+	return child
+}
---
+++ solve.go
@@ -0,0 +1,35 @@
+package main
+
+func Solve(table Table, depth int) MoveList {
+	return solveCore(NewContext(table, depth))
+}
+
+func solveCore(c *Context) MoveList {
+	if c.depth == 0 {
+		if c.table.IsSolution() {
+			return c.solution
+		}
+
+		return EmptyMoveList
+	}
+
+	for _, move := range c.possibleMoves {
+		if move == c.lastMove || !move.Valid(c.table) {
+			continue
+		}
+
+		table, ok := c.cache.Lookup(c.table, move)
+		if !ok {
+			table = c.table.Clone()
+			move.Apply(table)
+			c.cache.Record(c.table, move, table)
+		}
+
+		solution := solveCore(c.AppendSolution(table, move))
+		if len(solution) > 0 {
+			return solution
+		}
+	}
+
+	return EmptyMoveList
+}
